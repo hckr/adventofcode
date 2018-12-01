1f73dfb2ea37c549497e0d53a46e60ac6a69b1d4
Luke <luke@luke.co.pl>
2018-12-01 18:43:43 +0100

    [C#] 2018.1

---
+++ 2018/C#/1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var changes = new List<int>();
+            string line;
+            while(!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                changes.Add(int.Parse(line));
+            }
+
+            // part one
+            Console.WriteLine(changes.Sum());
+
+            // part two
+            int frequency = 0;
+            var previousFrequencies = new HashSet<int>();
+            previousFrequencies.Add(frequency);
+
+            while (true)
+            {
+                foreach (var change in changes)
+                {
+                    frequency += change;
+                    if (previousFrequencies.Contains(frequency))
+                    {
+                        Console.WriteLine(frequency);
+                        System.Environment.Exit(0);
+                    }
+                    previousFrequencies.Add(frequency);
+                }
+            }
+        }
+    }
+}
