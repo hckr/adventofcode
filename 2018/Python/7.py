095e29f091327e236d58051a95b0776491a3ec22
Luke <luke@luke.co.pl>
2018-12-08 00:22:55 +0100

    [Python] 2018.7 (part 2 and part 1 in terms of part 2)

--- 2018/Python/7.py
+++ 2018/Python/7.py
@@ -13,23 +13,46 @@ for dependency, task in re.findall(r'Step (.) must be finished before step (.) c
 tasks = list(sorted(set(tasks)))
 
 
-def try_do_task(task, done_tasks):
+def can_be_done(task, done_tasks):
     for dependency in dependencies.get(task, []):
         if dependency not in done_tasks:
             return False
     return True
 
 
-def order_tasks(tasks, dependencies):
+def order_tasks_workers(tasks, dependencies, workers=1, time_func=lambda task: 0):
+    tasks_count = len(tasks)
     tasks_left = tasks[:]
     done_tasks = []
-    while len(tasks_left) > 0:
-        for task in tasks_left:
-            if try_do_task(task, done_tasks):
-                tasks_left.remove(task)
-                done_tasks.append(task)
-                break
-    return done_tasks
+    workers_tasks = [None] * workers
+    workers_time_left = [1] * workers
+    time_elapsed = 0
+    while len(done_tasks) < tasks_count:
+        for w, time in enumerate(workers_time_left):
+            if time == 0:
+                if workers_tasks[w] is not None:
+                    done_tasks.append(workers_tasks[w])
+                    workers_tasks[w] = None
+        for w, task in enumerate(workers_tasks):
+            if task is None:
+                for task in tasks_left:
+                    if can_be_done(task, done_tasks):
+                        tasks_left.remove(task)
+                        workers_tasks[w] = task
+                        workers_time_left[w] = time_func(task)
+                        break
+        try:
+            subtrahend = min([t for t in workers_time_left if t > 0])
+            time_elapsed += subtrahend
+        except ValueError:  # min() arg is an empty sequence
+            pass
+        # print(time_elapsed, workers_tasks, done_tasks, workers_time_left, tasks_left)
+        for w, time in enumerate(workers_time_left):
+            if time > 0:
+                workers_time_left[w] -= subtrahend
+    return done_tasks, time_elapsed
 
 
-print(''.join(order_tasks(tasks, dependencies)))
+print(''.join(order_tasks_workers(tasks, dependencies)[0]))
+print(order_tasks_workers(tasks, dependencies, workers=5,
+      time_func=lambda task: ord(task) - 4)[1])
