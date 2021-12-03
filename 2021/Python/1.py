4f7e76efa1b00812c0799ffb1ad680ed0dde34af
Luke <luke@luke.co.pl>
2021-12-03 18:51:25 +0100

    [Python] 2021.* (add pytest and black, reformat files, extract code to main functions)

--- 2021/Python/1.py
+++ 2021/Python/1.py
@@ -1,13 +1,30 @@
 import fileinput
 
-measurements = [int(line) for line in fileinput.input()]
 
-# part 1
-print(sum(1 if measurements[i] > measurements[i-1] else 0
-          for i in range(1, len(measurements))))
+def main():
+    measurements = [int(line) for line in fileinput.input()]
 
-# part 2
-window_size = 3
-window_sums = [sum(measurements[i:i+window_size]) for i in range(len(measurements) - window_size + 1)]
-print(sum(1 if window_sums[i] > window_sums[i-1] else 0
-          for i in range(1, len(window_sums))))
+    # part 1
+    print(
+        sum(
+            1 if measurements[i] > measurements[i - 1] else 0
+            for i in range(1, len(measurements))
+        )
+    )
+
+    # part 2
+    window_size = 3
+    window_sums = [
+        sum(measurements[i : i + window_size])
+        for i in range(len(measurements) - window_size + 1)
+    ]
+    print(
+        sum(
+            1 if window_sums[i] > window_sums[i - 1] else 0
+            for i in range(1, len(window_sums))
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
--- 2021/Python/2.py
+++ 2021/Python/2.py
@@ -1,33 +1,39 @@
 import fileinput
 
-commands = [line.split() for line in fileinput.input()]
 
-# part 1
-horizontal = 0
-depth = 0
-for cmd, arg in commands:
-    if cmd == 'forward':
-        horizontal += int(arg)
-    elif cmd == 'down':
-        depth += int(arg)
-    elif cmd == 'up':
-        depth -= int(arg)
-    else:
-        raise RuntimeError(f'unknown cmd {cmd}')
-print(horizontal * depth)
+def main():
+    commands = [line.split() for line in fileinput.input()]
 
-# part 2
-horizontal = 0
-depth = 0
-aim = 0
-for cmd, arg in commands:
-    if cmd == 'down':
-        aim += int(arg)
-    elif cmd == 'up':
-        aim -= int(arg)
-    elif cmd == 'forward':
-        horizontal += int(arg)
-        depth += aim * int(arg)
-    else:
-        raise RuntimeError(f'unknown cmd {cmd}')
-print(horizontal * depth)
+    # part 1
+    horizontal = 0
+    depth = 0
+    for cmd, arg in commands:
+        if cmd == "forward":
+            horizontal += int(arg)
+        elif cmd == "down":
+            depth += int(arg)
+        elif cmd == "up":
+            depth -= int(arg)
+        else:
+            raise RuntimeError(f"unknown cmd {cmd}")
+    print(horizontal * depth)
+
+    # part 2
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for cmd, arg in commands:
+        if cmd == "down":
+            aim += int(arg)
+        elif cmd == "up":
+            aim -= int(arg)
+        elif cmd == "forward":
+            horizontal += int(arg)
+            depth += aim * int(arg)
+        else:
+            raise RuntimeError(f"unknown cmd {cmd}")
+    print(horizontal * depth)
+
+
+if __name__ == "__main__":
+    main()
